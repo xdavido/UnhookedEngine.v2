f511f132d6b2b26b68527df5d1e3fbc22bc54435
David Ruiz Luengo <davidruizluengo@gmail.com>
2023-12-28 19:17:32 +0100

    Rename Title

--- UnhookedEngine.v2/Globals.h
+++ UnhookedEngine.v2/Globals.h
@@ -67,7 +67,7 @@ enum update_status
 #define WIN_RESIZABLE true
 #define WIN_BORDERLESS false
 #define WIN_FULLSCREEN_DESKTOP false
-#define TITLE "Sheesh Engine"
+#define TITLE "Unhooked Engine"
 
 //Render
 #define VSYNC true
